033556d7260c733f995088f455f77d65616967d7
Evgenia <jezeld@mail.ru>
2023-02-05 00:21:00 +0300

    editbag

--- sctipts/script.js
+++ sctipts/script.js
@@ -1,11 +1,9 @@
 const aboutButton = document.querySelector('.profile__edit-button_type_about');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
-
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-job');
-
+let nameInput = formElement.querySelector('.popup__input_information_name');
+let jobInput = formElement.querySelector('.popup__input_information_job');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
