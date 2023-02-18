b888a07cccf35f95d06c030eb9c4c8273c444029
Evgenia <jezeld@mail.ru>
2023-02-18 18:12:47 +0300

    final

--- sctipts/script.js
+++ sctipts/script.js
@@ -28,25 +28,24 @@ const initialCards = [
 const aboutButton = document.querySelector('.profile__edit-button_type_about');
 const popup = document.querySelector('.popup');
 const formElementProfile = document.querySelector('.popup__form_type_edit'); // форма профайла
-const formElementAdd = document.querySelector('.popup__form_type_add');// форма добавления
+const formElementAdd = document.querySelector('.popup__form_type_add');// форма добавления карточек
 const nameInput = document.querySelector('.popup__input_information_name');
 const jobInput = document.querySelector('.popup__input_information_job');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 const places = document.querySelector('.places'); // секция карточек
-const formElementplace = document.querySelector('.popup__form-place');
 const template = document.querySelector('#card').content;
-const popupImagebig = document.querySelector('.popup-image');   //  картинки
-const popupEdit = document.querySelector('.popup-edit');   // профиля
-const popupAdd = document.querySelector('.popup-add');  // добавления
+const popupImagebig = document.querySelector('.popup-image');   //  картинкa big
+const popupEdit = document.querySelector('.popup-edit');   // профиль редактирования
+const popupAdd = document.querySelector('.popup-add');  // добавления User
 const closeButton = document.querySelectorAll('.popup__close');
 const buttonAdd = document.querySelector('.profile__add-button_type_add');
-const titleImg = document.querySelector('.popup__input_information_image');
-const linkImg = document.querySelector('.popup__input_information_link');
+const titleImgInput = document.querySelector('.popup__input_information_image');
+const linkImgInput = document.querySelector('.popup__input_information_link');
 
 closeButton.forEach((button) => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => toggleOpenPopup(popup));
+    const popup = button.closest('.popup');
+    button.addEventListener('click', () => toggleOpenPopup(popup));
 });
 
 const toggleOpenPopup = (popup) => {
@@ -59,23 +58,12 @@ const handleAboutButtonClick = () => {
     jobInput.value = profileAbout.textContent;
 };
 
-
-const handleOverlyClick = (event) => {
-    if (event.target === event.currentTarget) {
-        toggleOpenPopup(popup);
-    }
-};
-
-function handleFormSubmit(evt) {
-    evt.preventDefault();
+function handleFormSubmit(event) {
+    event.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
     toggleOpenPopup(popupEdit);
 };
-
-aboutButton.addEventListener('click', handleAboutButtonClick);
-popup.addEventListener('click', handleOverlyClick);
-formElementProfile.addEventListener('submit', handleFormSubmit);
 
 const makeCard = (item) => {
     const card = template.querySelector('.place').cloneNode(true);
@@ -89,10 +77,10 @@ const makeCard = (item) => {
         event.target.classList.toggle('place__like_active');
     });
     card.querySelector('.place__urn').addEventListener('click', function () {
-       card.remove();
+        card.remove();
     });
-    cardImage.addEventListener('click', function() {
-        const popupImg =  document.querySelector('.popup__img');
+    cardImage.addEventListener('click', function () {
+        const popupImg = document.querySelector('.popup__img');
         const popupHeading = document.querySelector('.popup__heading');
         popupImg.src = item.link;
         popupImg.alt = item.name;
@@ -114,9 +102,20 @@ buttonAdd.addEventListener('click', () => {
     toggleOpenPopup(popupAdd);
 });
 
+formElementAdd.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const cardUser = {
+        link: linkImgInput.value,
+        name: titleImgInput.value
+    };
+    renderCard(places, cardUser);
+    linkImgInput.value = '';
+    titleImgInput.value = '';
+    toggleOpenPopup(popupAdd);
+});
 
-
-
+aboutButton.addEventListener('click', handleAboutButtonClick);
+formElementProfile.addEventListener('submit', handleFormSubmit);
 
 
 
